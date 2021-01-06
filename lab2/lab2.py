c41badda1a9bf6e7107dd8b3f73e676b43a8b3ca
Dhanaraj <56156360+dhanaraj1999@users.noreply.github.com>
2021-01-06 15:35:16 +0530

    Update lab2.py

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -104,3 +104,51 @@ def candidate_elimination(examples):
         print("S[{0}]:".format(i), S)
     return
 candidate_elimination(examples)
+
+
+
+
+easy prg
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  6 15:03:15 2021
+
+@author: asus
+"""
+import csv
+a = []
+with open('EnjoySport.csv','r') as f:
+    reader = csv.reader(f)
+    for i in reader:
+        a.append(i)
+
+n = len(a[0]) - 1
+print("Initially")
+S = ["0"] * n
+G = ["?"] * n
+print("S0: {0}".format(S))
+print("G0: {0}".format(G))
+S = a[0][:-1] #first training example
+tmp = []
+for i,row in enumerate(a):
+    if row[-1] == "Yes":
+        for j,attrib in enumerate(row[:-1]):
+            if attrib != S[j]:
+                S[j] = "?"
+            for k,g in enumerate(tmp):
+                if g[j] != "?" and g[j] != S[j]:
+                    del tmp[k]
+
+    else:
+        for j,attrib in enumerate(row[:-1]):
+            if attrib != S[j] and S[j] != "?":
+                G[j] = S[j]
+                tmp.append(G)
+                G = ["?"] * n
+    print("-----------------------------------------------------------")
+    print("For Training example {0} ".format(i+1,S))
+    print("S{0}: {1}".format(i+1,S))
+    if(tmp==[]):
+        print("G{0}: {1}".format(i+1,G))
+    else:
+        print("G{0}: {1}".format(i+1,tmp))
